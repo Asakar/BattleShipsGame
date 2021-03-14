1a0bec8194c2fdf0f71e08b01c658abbbdfb238a
Asakar <asakarh@gmail.com>
2021-03-14 16:26:19 +0000

    first commit

---
+++ src/contoller/GameController.java
@@ -0,0 +1,4 @@
+package contoller;
+
+public class GameController {
+}
---
+++ src/model/Destoyer.java
@@ -0,0 +1,10 @@
+package model;
+
+public class Destoyer {
+
+    private int size;
+
+    public Destoyer(int size) {
+        this.size = size;
+    }
+}
